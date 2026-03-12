78e3a82bc87a8a1f3da79ac76a311cb53a26273c
me2015gojo <iammjgod@gmail.com>
2026-03-12 12:26:52 +0530

    initial commit

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
 print('HELLO')
+
+print(2333*2)
+
+print('mrityunjay\njjk')
+
+name='mrityunjay' \
+''
+age=10
+percentage=99.9
+
+print('my name is ?',name)
+
+#add
+
+n1=int(input('enter the first no.'))
+n2=int(input('please enter the second no.'))
+print(n1+n2)
+
