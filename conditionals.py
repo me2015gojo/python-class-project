3bc6d1185f573e8660d3dcdfc687be4007d61ab0
me2015gojo <iammjgod@gmail.com>
2026-03-19 12:05:33 +0530

    initial.commit

---
+++ conditionals.py
@@ -0,0 +1,31 @@
+# write a program to check whether the given number is positive?
+
+# num=int(input('enter number1'))
+# if num>0:
+#     print(num," is a positive number")
+# Write to check if a person is buying oranges at 100 rs and later selling it 1at 120 rs. Find that he has profit or loss?
+
+# boughtprice=100
+# sellprice=20
+
+# if boughtprice<sellprice:
+#     print("you have profit of rs",(sellprice-boughtprice))
+# else:
+#     print('you have loss of rs',boughtprice-sellprice)
+
+# Write a program to check whether the given number is even or odd?
+
+# num1=int(input('enter number:'))
+
+# if num1%2==0:
+#     print(' this is a even number')
+# else:
+#     print(' this is a odd number')
+
+# Write a program to check whether the given number is greater than 15 or smaller than 15.
+
+num1=int(input('enter a number:'))
+if num1<15:
+    print('this is smaller than 15')
+else:
+    print('this is larger than fifteen')
