e540f77cc2f4afa59e94c622f3ae24071724bd85
me2015gojo <iammjgod@gmail.com>
2026-03-26 12:18:14 +0530

    initial commit

---
+++ condirnals1.py
@@ -0,0 +1,25 @@
+# x=[1,2,3]
+# y=[1,2,3]
+
+# print(x is y)
+
+# A= -23467347556676563
+# B=5442384876472384284
+
+# print(A is not B)
+
+# Write a program to apply the right shift and left shift bitwise operator.
+
+a= 10
+b=-10
+
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+c= 5
+d=-10
+
+print("c << 1 =", c >> 1)
+print("d << 1 =", d >> 1)
+
+
---
+++ operatorsz.py
@@ -0,0 +1,38 @@
+# a=7
+# b=6
+# c=5
+# d=56
+# e=456
+# f=678
+# z=(e+f)* a/d
+# print('value of (e+f)* a/d  ', z)
+
+# name='john'
+# age= 30000
+
+# if name== 'alex' or name=='john' and age >=2 :
+#     print('hello welcome')
+# else:
+#     print('goodbye')
+
+# n=int(input('enter a numerator        '))
+# d=int(input('enter a denominator       '))
+
+# if n%d==0:
+#     print('it is divisible')
+# else:
+#     print('it is not divisible')
+
+mean1=38
+wrong_number= 36
+correct_number=56
+total_number=40
+
+sum=mean1*total_number
+print("the sum of 40 number:",sum)
+
+num2=sum-((wrong_number)-(correct_number))
+print('sum-((wrong_number)-(correct_number))  ', num2)
+
+mean2=num2/total_number
+print(mean2)
