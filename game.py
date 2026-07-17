b540762ed4609af1db10f46c96a188f1f7eab8bc
me2015gojo <iammjgod@gmail.com>
2026-07-17 12:38:03 +0530

    initial commit

---
+++ game.py
@@ -0,0 +1,13 @@
+import math
+import randomn
+import pygame
+screen_width=800
+screen_height = 500
+player_startx=370
+player_starty=370
+enemy_startymin=50
+enemy_startymax=150    
+enemyspeedx=1000
+enemyspeedy=1
+bulletspeed=1500
+collison_distance=27
